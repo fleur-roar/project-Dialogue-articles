7dc1cec1d11b9c8964978c9ec2ee64cc6147207d
masaoka-yukiko <72147821+masaoka-yukiko@users.noreply.github.com>
2021-05-07 19:22:01 +0300

    Update get_tables.py

--- get_tables.py
+++ get_tables.py
@@ -1,4 +1,4 @@
 import tabula
 pdf_path = "D:/programming/project/anastasyevdg-147.pdf"
-x = tabula.read_pdf(pdf_path, pages = "all", multiple_tables = True)
-print(x)
+tables = tabula.read_pdf(pdf_path, pages = "all", multiple_tables = True)
+print(tables)
