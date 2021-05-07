af1a5086b970257f36e94046970965a00e012f72
masaoka-yukiko <72147821+masaoka-yukiko@users.noreply.github.com>
2021-05-07 19:26:48 +0300

    Update pdf_2_text.py

--- pdf_2_text.py
+++ pdf_2_text.py
@@ -1,11 +1,11 @@
 import pdfminer.high_level
 
 pdfdir = 'D:/programming/project/anastasyevdg-147.pdf'
-x = pdfminer.high_level.extract_text(pdfdir)
-print(x)
+textpdf = pdfminer.high_level.extract_text(pdfdir)
+print(textpdf)
 
 contents = []
-for line in x.split('\n'):
+for line in textpdf.split('\n'):
     if line.startswith('1.') or line.startswith('2.') or line.startswith('3.') or line.startswith('4.') or line.startswith('5.') or line.startswith('6.') or line.startswith('7.') or line.startswith('8.') or line.startswith('9.'):
         contents.append(line)
 print(contents)
